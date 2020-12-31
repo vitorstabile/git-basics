4df3674969de7b1c19aa72d0b51e933409f856c1
Vitor Garcia <vitor.stabile@hotmail.com>
2020-12-31 19:45:33 -0300

    class 1 push

---
+++ class1-git-commit/class1-git-commit.bash
@@ -0,0 +1,27 @@
+## basic commit
+
+## make a git bash here in the main folder of the project
+
+git init
+
+# verify if you name and user in config
+
+git config --list
+
+## if you don't have a user in yout git terminal
+
+git config --global user.name "Your name"
+
+git config --global user.email "name@gmail.com"
+
+git status
+
+git add .
+
+git status
+
+git commit -m "Project Created"
+
+git remote add origin https://github.com/vitorstabile/git-basics.git
+
+git push -u origin master
