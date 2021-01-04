fb5fff46d0f6ab2429beabd065845ef82c070525
Vitor Garcia <vitor.stabile@hotmail.com>
2021-01-04 10:33:24 -0300

    Change class 1 - Origin Master to Main command

--- class1-create-project/class1-create-project.bash
+++ class1-create-project/class1-create-project.bash
@@ -4,7 +4,7 @@
 
 git init
 
-# verify if you name and user in config
+## verify if you name and user in config
 
 git config --list
 
@@ -24,4 +24,14 @@ git commit -m "Project Created"
 
 git remote add origin https://github.com/vitorstabile/git-basics.git
 
+## If your git is in (master) try this command
+
 git push -u origin master
+
+## To change to the new format
+
+git branch -M main
+
+## This will change the origin from (master) to (main)
+
+git push -u origin main
